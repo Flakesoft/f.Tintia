1f38e460bb8e71f3bc51e16a48d619334f6a804c
Zoran <zoki@tutanota.de>
2026-07-26 15:38:06 +0200

    Create LoadingState widget for loading indication
    Added a LoadingState widget to display a loading icon and message.

--- lib/widgets/loading_state.dart
+++ lib/widgets/loading_state.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+class LoadingState extends StatelessWidget {
+  const LoadingState({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      key: const ValueKey('loading'),
+      children: [
+        Icon(
+          Icons.hourglass_top,
+          size: 72,
+          color: Theme.of(context)
+              .colorScheme
+              .primary,
+        ),
+
+        const SizedBox(
+          height: 20,
+        ),
+
+        Text(
+          'Loading image...',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium,
+        ),
+      ],
+    );
+  }
+}
