622d8a7835d67e86774b7d86910c065be0a3b2eb
Zoran <batajeremiccc@gmail.com>
2026-07-30 11:47:02 +0200

    Update home_screen.dart [build]

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+
 import '../layout/adaptive_layout.dart';
 import '../models/image_state.dart';
 import '../services/image_picker_service.dart';
 import '../services/image_processing_service.dart';
-import '../widgets/color_info_card.dart';
-import '../widgets/empty_state.dart';
-import '../widgets/image_viewer.dart';
-import '../widgets/loading_state.dart';
+import '../layout/mobile_home_layout.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -146,105 +144,18 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title:
             const Text('f.Tintra'),
-        centerTitle: true,
+        centerTitle:
+            true,
       ),
-
       body: AdaptiveLayout(
-        mobile: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(24),
-
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxWidth: 700,
-              ),
-
-              child: Column(
-                mainAxisAlignment:
-                    MainAxisAlignment.center,
-
-                children: [
-                  AnimatedSwitcher(
-                    duration:
-                        const Duration(
-                          milliseconds: 250,
-                        ),
-
-                    child: isLoading
-
-                        ? const LoadingState()
-
-                        : imageState != null
-
-                            ? ImageViewer(
-                                key:
-                                    const ValueKey(
-                                  'image',
-                                ),
-
-                                imageState:
-                                    imageState!,
-
-                                imageKey:
-                                    _imageKey,
-
-                                transformationController:
-                                    _transformationController,
-
-                                onTap:
-                                    _onImageTap,
-                              )
-
-                            : const EmptyState(),
-                  ),
-
-                  const SizedBox(
-                    height: 24,
-                  ),
-
-                  if (imageState?.selectedColor != null)
-
-                    ColorInfoCard(
-                      color:
-                          imageState!.selectedColor!,
-                    )
-
-                  else if (imageState != null)
-
-                    Text(
-                      'Tap the image to pick a color',
-
-                      style:
-                          Theme.of(context)
-                              .textTheme
-                              .bodyMedium,
-                    ),
-
-                  const SizedBox(
-                    height: 32,
-                  ),
-
-                  FilledButton.icon(
-                    onPressed:
-                        isLoading
-                            ? null
-                            : _selectImage,
-
-                    icon:
-                        const Icon(
-                      Icons.image,
-                    ),
-
-                    label: Text(
-                      imageState == null
-                          ? 'Select image'
-                          : 'Choose another image',
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+        mobile: MobileHomeLayout(
+          imageState: imageState,
+          isLoading: isLoading,
+          imageKey: _imageKey,
+          transformationController:
+              _transformationController,
+          onImageTap: _onImageTap,
+          onSelectImage: _selectImage,
         ),
       ),
     );
