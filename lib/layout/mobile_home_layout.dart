1d8b4eb82c1c350f2f6b075aa199274928091adf
Zoran <zoki@tutanota.de>
2026-08-01 17:23:01 +0200

    Refactor MobileHomeLayout to use HomeContent widget

--- lib/layout/mobile_home_layout.dart
+++ lib/layout/mobile_home_layout.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../models/image_state.dart';
-import '../widgets/color_info_card.dart';
-import '../widgets/empty_state.dart';
-import '../widgets/image_viewer.dart';
-import '../widgets/loading_state.dart';
+import '../widgets/home_content.dart';
 
 class MobileHomeLayout extends StatelessWidget {
   final ImageState? imageState;
   final bool isLoading;
   final GlobalKey imageKey;
   final TransformationController transformationController;
-  final ValueChanged<TapUpDetails> onImageTap;
+  final GestureTapUpCallback onImageTap;
   final VoidCallback onSelectImage;
 
   const MobileHomeLayout({
@@ -26,100 +23,14 @@ class MobileHomeLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SingleChildScrollView(
-        padding:
-            const EdgeInsets.all(24),
-
-        child: ConstrainedBox(
-          constraints:
-              const BoxConstraints(
-            maxWidth: 700,
-          ),
-
-          child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.center,
-
-            children: [
-              AnimatedSwitcher(
-                duration:
-                    const Duration(
-                      milliseconds: 250,
-                    ),
-
-                child: isLoading
-                    ? const LoadingState()
-
-                    : imageState != null
-                        ? ImageViewer(
-                            key:
-                                const ValueKey(
-                              'image',
-                            ),
-
-                            imageState:
-                                imageState!,
-
-                            imageKey:
-                                imageKey,
-
-                            transformationController:
-                                transformationController,
-
-                            onTap:
-                                onImageTap,
-                          )
-
-                        : const EmptyState(),
-              ),
-
-              const SizedBox(
-                height: 24,
-              ),
-
-              if (imageState?.selectedColor !=
-                  null)
-                ColorInfoCard(
-                  color:
-                      imageState!
-                          .selectedColor!,
-                )
-
-              else if (imageState != null)
-                Text(
-                  'Tap the image to pick a color',
-                  style:
-                      Theme.of(context)
-                          .textTheme
-                          .bodyMedium,
-                ),
-
-              const SizedBox(
-                height: 32,
-              ),
-
-              FilledButton.icon(
-                onPressed:
-                    isLoading
-                        ? null
-                        : onSelectImage,
-
-                icon:
-                    const Icon(
-                  Icons.image,
-                ),
-
-                label: Text(
-                  imageState == null
-                      ? 'Select image'
-                      : 'Choose another image',
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return HomeContent(
+      isLoading: isLoading,
+      imageState: imageState,
+      imageKey: imageKey,
+      transformationController:
+          transformationController,
+      onImageTap: onImageTap,
+      onSelectImage: onSelectImage,
     );
   }
 }
